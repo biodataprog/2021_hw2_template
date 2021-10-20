8bdd8077b1e886e54fedaa3dbf6f9abedf4fabd3
Jason Stajich <jasonstajich.phd@gmail.com>
2021-10-20 14:56:30 -0700

    add template for problem 1

---
+++ squared_cubed.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+# you can change this for experimentation
+MaxNum = 30
+
+# this will print a header
+print("\t".join(["N","Squared","Cubed"]))
+
+# write your code below you can use
+# range(MaxNum)
+# as a way to get numbers from 0...MaxNum
+# eg
+# for n in range(MaxNum):
+#    # code here
+# to convert a number into strings
+# numstring = str(number)
+# or
+# numstring = "%d"%(number)
+# if you wanted to show scientific notation when num get very large
+# numstring = "%g"%(number)
